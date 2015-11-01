a67917e0cc32a267f52deabee0dd7fe8eda6b672
rafiki <mark@rafiki>
2015-11-02 00:44:40 +0100

    load configuration; show through command line

--- source/cmd.py
+++ source/cmd.py
@@ -1,8 +1,11 @@
 
 from argparse import ArgumentParser, FileType
-from sys import argv
+from json import dump
+from os import makedirs
+from os.path import dirname, exists
+from sys import argv, stderr
 from tempfile import tempdir
-from versions import regex_str
+from conf import settings
 
 
 def parse(args):
@@ -15,7 +18,7 @@ def parse(args):
 		help='The name of the output directory (or base name of the output file if --single-file is used).')
 	parser.add_argument('--review-mode', dest='review', action='store_true',
 		help='Create the document in review mode, allowing readers to leave corrections and comments.')
-	parser.add_argument('--version', dest='version', type=regex_str(r'(?:\*|[0-9]+)'), default='*',
+	parser.add_argument('--version', dest='version', type=str, default='*',  # maybe use a fancy regex data type
 		help='Request a specific compiler version, usable for consistent results. Likely to abort if the current version isn\'t correct.')
 	parser.add_argument('--minimize', dest='minimize', action='store_true',
 		help='Decrease the output filesize at the cost of readability by removing whitespace and merging styles and scripts. Side effects are possible; make sure to check!.')  # e.g. js strict mode
@@ -27,12 +30,30 @@ def parse(args):
 		help='Do a clean compile, ignoring existing temporary files.')
 	parser.add_argument('--tempdir', dest='tempdir', type=str, default=tempdir,
 		help='The directory to store temporary files')
+	parser.add_argument('--show_conf', dest='show_conf', action='store_true',
+		help='Print all configuration values including defaults to the screen.')  # todo: this one should also be available at notexpm
+	parser.add_argument('--make_conf', dest='make_conf', action='store_true',
+		help='Make a default configuration file.')  # todo: this one should also be available at notexpm
 	opts = parser.parse_args(args)
+	if opts.show_conf:  # todo: move this code somewhere else
+		print(settings.get_config_string())
+	if opts.make_conf:
+		conf_path = settings.get_user_config_location()
+		if exists(conf_path):
+			stderr.write('Configuration file already exists at "{0:s}"; not making any changes.\n'.format(conf_path))
+			exit()
+		else:
+			conf = settings.get_defaults()
+			makedirs(dirname(conf_path), exist_ok=True)
+			with open(conf_path, 'w+') as fh:
+				dump(conf, fp=fh, indent=2, sort_keys=False)
+	if opts.show_conf or opts.make_conf:
+		exit()
 	if opts.minimize:
 		print('--minimize is not yet implemented; option ignored')
 	if opts.review:
 		print('--review-mode is not yet implemented; option ignored')
-	if opts.version:
+	if opts.version != '*':
 		print('--version is not yet implemented; option ignored')
 	if opts.remote:
 		print('--remote is not yet implemented; option ignored')
